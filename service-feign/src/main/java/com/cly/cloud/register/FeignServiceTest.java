6e2a150222be8175f34e29c94bc9a2a9b87029e6
zheng jiang <jiang.zheng@msxf.com>
2017-09-27 18:06:28 +0800

    新增zuul路由转发和过滤器

--- service-feign/src/main/java/com/cly/cloud/register/FeignServiceTest.java
+++ service-feign/src/main/java/com/cly/cloud/register/FeignServiceTest.java
@@ -1,6 +1,7 @@
 package com.cly.cloud.register;
 
 import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestParam;
  * <li>Version: 1.0</li>
  * <li>Content: create</li>
  */
+@Service
 @FeignClient(value = "service-test", fallback = SchedualServiceHiHystric.class)
 public interface FeignServiceTest {
 
---
+++ service-zuul/src/main/java/com/cly/cloud/filter/MyFilter.java
@@ -0,0 +1,58 @@
+package com.cly.cloud.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @ Filename MyFilter.java
+ * @ Description
+ * @ Version 1.0
+ * <li>@author: jiang.zheng</li>
+ * <li>Date: 2017/9/27 17:50</li>
+ * <li>Version: 1.0</li>
+ * <li>Content: create</li>
+ */
+@Service
+public class MyFilter extends ZuulFilter {
+    private static Logger log = LoggerFactory.getLogger(MyFilter.class);
+
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() {
+        RequestContext ctx = RequestContext.getCurrentContext();
+        HttpServletRequest request = ctx.getRequest();
+        log.info(String.format("%s >>> %s", request.getMethod(), request.getRequestURL().toString()));
+        Object accessToken = request.getParameter("token");
+        if(accessToken == null) {
+            log.warn("token is empty");
+            ctx.setSendZuulResponse(false);
+            ctx.setResponseStatusCode(401);
+            try {
+                ctx.getResponse().getWriter().write("token is empty");
+            }catch (Exception e){}
+
+            return null;
+        }
+        log.info("ok");
+        return null;
+    }
+}
